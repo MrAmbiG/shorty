7e5969233bdbc75724d44c5ec8236032edccd33d
gajeambi <amd@ubuntuserver-18-x8664.local.lan>
2020-12-29 10:33:05 -0500

    django outside container not working

--- dockerize.sh
+++ dockerize.sh
@@ -1,1 +1,2 @@
-docker build --no-cache  -t shorty -f ./Deployment/Dockerfile .
+# docker build --no-cache  -t shorty -f ./Deployment/Dockerfile .
+docker build -t shorty -f ./Deployment/Dockerfile .
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 # daphne shorty.asgi:application -b 0.0.0.0 -p 8001
-daphne shorty.asgi:application -b 0.0.0.0 -p 8001
+daphne shorty.asgi:application -b 0 -p 8001

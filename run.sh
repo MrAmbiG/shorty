193070eb217e67e20c5e5c46312a3a5f59f09ded
gajeambi <amd@ubuntuserver-18-x8664.local.lan>
2020-12-29 08:27:38 -0500

    static and app server working

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ cd shorty
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 127.0.0.1:8000
+daphne shorty.asgi:application -b 0.0.0.0 -p 8001
+# python manage.py runserver 0.0.0.0:8001
--- runDevServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd shorty
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 127.0.0.1:8000

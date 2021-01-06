e08bdfc5ab5495eb9c6bac343b5b13ced22cb869
Gajendra D Ambi <gajuambi@gmail.com>
2021-01-06 07:34:25 +0530

    no diff bn daphne and dev server

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-gajuambi#!/bin/bash
+#!/bin/bash
 cd shorty
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0 8000
-# daphne shorty.asgi:application -b 0 -p 8000
+# python manage.py runserver 0.0.0.0 8001
+daphne shorty.asgi:application -b 0 -p 8001

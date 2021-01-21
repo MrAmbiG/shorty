29eab9f11e91613014a0058539128fdc4b94cf3b
Gajendra D Ambi <gajuambi@gmail.com>
2021-01-22 04:05:17 +0530

    temp save

--- shorty/core/forms.py
+++ shorty/core/forms.py
@@ -3,6 +3,19 @@ from .models import shorties
 
 
 class shorties_form(forms.ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.label_suffix = ""  # Removes : as label suff
+    #     self.fields['original'].label = '' # removes label
     class Meta:
         model = shorties
         fields = ["original"]
+        # widgets = {
+        #     'original': forms.TextInput(attrs={'placeholder': 'Enter URL'}),
+        # }
+
+    # def clean(self):
+    #     cleaned_data = super(ContactForm, self).clean()
+    #     original = cleaned_data.get('original')
+    #     if not original:
+    #         raise forms.ValidationError('Input a URL')
